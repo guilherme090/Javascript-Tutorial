2c166ce49c23cc81c301e3ed46725bd0f2ca8995
Guilherme Simoes <guilherme-elec12@hotmail.com>
2020-04-17 12:25:07 -0300

    <feature> Did exercises about variable naming
    Created variables and constants.
    Showed their values in alerts.

--- scriptFile.js
+++ scriptFile.js
@@ -1,2 +1,14 @@
 "use strict";
-alert("I'm JavaScript!")
+//JavaScript Fundamentals Exercises
+//Creating variables
+let admin;
+let name;
+name = 'John';
+admin = name;
+//Giving the right name
+const OUR_PLANET = "Earth";
+const visitorName = "John";
+alert("I'm JavaScript!");
+alert("The admin's name is " + name);
+alert("Our planet's name is " + OUR_PLANET);
+alert("The visitor's name is " + visitorName);
