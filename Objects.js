2a39a396dd3817e49f869d66cbb9d4ba5c3c6c10
Guilherme Simoes <guilherme-elec12@hotmail.com>
2020-07-10 00:04:56 -0300

    <feature> did last exercises in the Objects: the basics section. New Calculator and Accumulator functions.

--- Objects.js
+++ Objects.js
@@ -173,3 +173,160 @@ function makeUserThatWorks(){
 	};
 };
 
+/*
+Create a calculator
+
+importance: 5
+Create an object calculator with three methods:
+read() prompts for two values and saves them as object properties.
+sum() returns the sum of saved values.
+mul() multiplies saved values and returns the result.
+let calculator = {
+  // ... your code ...
+};
+
+calculator.read();
+alert( calculator.sum() );
+alert( calculator.mul() );
+*/
+
+let calculator = {
+	value1: 0,
+	value2: 0,
+
+	read: function(){
+		value1 = parseInt(prompt("Value 1: ","0"));
+		value2 = parseInt(prompt("Value 2: ","0"));
+	},
+	
+	sum: function(){
+		return value1 + value2;
+	},
+
+	mul: function(){
+		return value1 * value2;
+	}
+};
+
+calculator.read();
+alert( calculator.sum() );
+alert( calculator.mul() );
+
+/*
+Chaining
+
+importance: 2
+There’s a ladder object that allows to go up and down:
+let ladder = {
+  step: 0,
+  up() {
+    this.step++;
+  },
+  down() {
+    this.step--;
+  },
+  showStep: function() { // shows the current step
+    alert( this.step );
+  }
+};
+
+Now, if we need to make several calls in sequence, can do it like this:
+ladder.up();
+ladder.up();
+ladder.down();
+ladder.showStep(); // 1
+
+
+Modify the code of up, down and showStep to make the calls chainable, like this:
+ladder.up().up().down().showStep(); // 1
+
+Such approach is widely used across JavaScript libraries.
+*/
+
+let ladder = {
+	step: 0,
+	up() {
+	  this.step++;
+	  return this;
+	},
+	down() {
+	  this.step--;
+	  return this;
+	},
+	showStep: function() { // shows the current step
+	  alert( this.step );
+	  return this;
+	}
+  };
+
+  ladder.up().up().down().showStep();
+
+/*
+Create new Calculator
+
+importance: 5
+Create a constructor function Calculator that creates objects with 3 methods:
+read() asks for two values using prompt and remembers them in object properties.
+sum() returns the sum of these properties.
+mul() returns the multiplication product of these properties.
+For instance:
+let calculator = new Calculator();
+calculator.read();
+
+alert( "Sum=" + calculator.sum() );
+alert( "Mul=" + calculator.mul() );
+*/  
+
+function NewCalculator(){
+	this.value1 = 0;
+	this.value2 = 0;
+	this.read = function(){
+		value1 = Number(prompt("Value 1: ","0"));
+		value2 = Number(prompt("Value 2: ","0"));
+	},
+	this.sum = function(){
+		return value1 + value2;
+	},
+
+	this.mul = function(){
+		return value1 * value2;
+	}
+}
+
+let newCalc = new NewCalculator();
+newCalc.read();
+alert("Sum= " + newCalc.sum() );
+alert("Mul= " + newCalc.mul() );
+
+/*
+Create new Accumulator
+
+importance: 5
+Create a constructor function Accumulator(startingValue).
+Object that it creates should:
+Store the “current value” in the property value. The starting value is set to the argument of the constructor startingValue.
+The read() method should use prompt to read a new number and add it to value.
+In other words, the value property is the sum of all user-entered values with the initial value startingValue.
+Here’s the demo of the code:
+let accumulator = new Accumulator(1); // initial value 1
+
+accumulator.read(); // adds the user-entered value
+accumulator.read(); // adds the user-entered value
+
+alert(accumulator.value); // shows the sum of these values
+*/
+
+function Accumulator(startingValue){
+	this.currentValue = Number(startingValue);
+	this.read = function(){
+		newNumber = Number(prompt("What value would you like to add to current value?","0"));
+		this.currentValue += newNumber;
+	};
+}
+
+let accumulator = new Accumulator(1); // initial value 1
+
+accumulator.read(); // adds the user-entered value
+accumulator.read(); // adds the user-entered value
+
+alert(accumulator.currentValue); // shows the sum of these values
